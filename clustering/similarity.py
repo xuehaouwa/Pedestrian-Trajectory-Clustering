3c7f927b2d5a0e04a3d42007efc9815c60814a93
Hao Xue <xuehaoperth@gmail.com>
2018-02-07 10:13:08 +0800

    Add trajectory similarity method

---
+++ clustering/similarity.py
@@ -0,0 +1,72 @@
+import math
+import numpy as np
+
+
+# def is_closed(p1, p2, detect_radius):
+#     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) < detect_radius
+#
+#
+# def similarity(r1, r2, detect_radius):
+#     count = 0.0
+#
+#     for i in range(len(r1)):
+#         is_closed_flag = False
+#         for j in range(len(r2)):
+#             if is_closed(r1[i], r2[j], detect_radius):
+#                 is_closed_flag = True
+#
+#         if is_closed_flag:
+#             count += 1.0
+#
+#     return count / len(r1)
+
+
+class Cluster():
+    def __init__(self, detect_radius, similarity_threashold, data):
+        self.detect_radius = detect_radius
+        self.similarity_threashold = similarity_threashold
+        self.data = data
+        self.similarity_matrix = None
+
+    def is_closed(self, p1, p2):
+        """
+
+        :param p1: point 1
+        :param p2: point 2
+        :return:
+        """
+        return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) < self.detect_radius
+
+    def cal_similarity(self, r1, r2):
+        count = 0.0
+
+        for i in range(len(r1)):
+            is_closed_flag = False
+            for j in range(len(r2)):
+                if self.is_closed(r1[i], r2[j]):
+                    is_closed_flag = True
+
+            if is_closed_flag:
+                count += 1.0
+
+        return count / len(r1)
+
+    def cal_similarity_matrix(self):
+        self.similarity_matrix = np.zeros((len(self.data), len(self.data)))
+
+        for i1, r1 in enumerate(self.data):
+            for i2, r2 in enumerate(self.data):
+                self.similarity_matrix[i1][i2] = self.cal_similarity(r1, r2)
+
+    def clustering(self):
+        clusters = [0] * len(self.similarity_matrix)
+        i = 1
+        for x, row in enumerate(self.similarity_matrix):
+            for y, item in enumerate(row):
+                if item >= self.similarity_threashold and self.similarity_matrix[y][x] >= self.similarity_threashold:
+                    if clusters[x] != 0:
+                        clusters[y] = clusters[x]
+                    else:
+                        clusters[y] = clusters[x] = i
+                        i = i + 1
+        return clusters
--- town_clustering.py
+++ town_clustering.py
@@ -9,6 +9,11 @@ from data.process_data import DataLoader
 import numpy as np
 from utils.drawing import DrawTrajectory
 import time
+from clustering.similarity import Cluster
+
+detect_radius = 100
+similarity_threashold = 0.5
+
 
 def town_data():
     DL = DataLoader('./data/TownCentre-groundtruth.txt')
@@ -26,17 +31,23 @@ def town_data():
 
 town_trajectories = town_data()
 
-cluster = Clustering()
+# cluster = Clustering()
+#
+# start_time = time.time()
+# res = cluster.clusterSpectral(town_trajectories[0: 184], 6)
+#
+# print(time.time() - start_time)
+#
+# print(res)
+#
+# drawer = DrawTrajectory('town_background.png')
+# drawer.draw_clusterd_trajectories(town_trajectories[0: 184], res, 6)
 
-start_time = time.time()
-res = cluster.clusterSpectral(town_trajectories[0: 184], 6)
+clustering = Cluster(detect_radius, similarity_threashold, town_trajectories[0: 30])
 
+clustering.cal_similarity_matrix()
 
-print(time.time()-start_time)
-
-print(res)
+clusters = clustering.clustering()
 
 drawer = DrawTrajectory('town_background.png')
-drawer.draw_clusterd_trajectories(town_trajectories[0: 184], res, 6)
-
-
+drawer.draw_clusterd_trajectories(town_trajectories[0: 30], clusters, 6)
